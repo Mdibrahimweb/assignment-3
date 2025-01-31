8b04c954dbce2eb68e0d501c74b19ef1bfa46cdf
Md Ibrahim <lsp2020e@gmail.com>
2025-01-31 18:57:24 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+  },
+};
